2a4db34b2efb50dec7b36719ef997e36d9fca0c2
Eduard Mazo <eduard.mazo@gmail.com>
2021-04-02 00:32:56 -0500

    Refactoring code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # go-temp
 API for iot Temp
 
-BMP-280 sensor and Wemmos D1
+BMP-280 sensor and Wemos D1
--- main.go
+++ main.go
@@ -11,13 +11,30 @@ import (
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func dbHandled(temp float64, sensorID int) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+type sensor struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty"`
+	sensorID   int                `bson:"sensorId,omitempty"`
+	numSamples int                `bson:"numSamples,omitempty"`
+	day        string             `bson:"day,omitempty"`
+	first      float64            `bson:"first,omitempty"`
+	last       float64            `bson:"last,omitempty"`
+	samples    []sample           `bson:"samples,omitempty"`
+}
+
+type sample struct {
+	val  float64 `bson:"val,omitempty"`
+	time int64   `bson:"time,omitempty"`
+}
+
+func main() {
+	/*ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()*/
+	ctx := context.Background()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
 		"mongodb+srv://EASC_01:0KzQlxBvEk8yDmLV@thundersandbox.v0ydj.mongodb.net/ThunderSandBox?retryWrites=true&w=majority",
 	))
@@ -25,62 +42,13 @@ func dbHandled(temp float64, sensorID int) {
 		log.Fatal(err)
 	}
 	bmp280DB := client.Database("bmp280")
-	collNames, err := bmp280DB.ListCollectionNames(ctx, bson.M{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(collNames)
 	locations := bmp280DB.Collection("locations")
-	/*locationAdd, err := locations.InsertOne(ctx, bson.D{
-		{Key: "title", Value: "The Polyglot Developer Podcast"},
-		{Key: "author", Value: "Nic Raboy"},
-		{Key: "tags", Value: bson.A{"development", "programming", "coding"}},
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(locationAdd)
-	locationAdd, err = locations.InsertOne(ctx, bson.D{
-		{Key: "sensorID", Value: 1234},
-		{Key: "numSamples", Value: 1},
-		{Key: "samples", Value: bson.A{
-			bson.D{
-				{Key: "val", Value: 70},
-				{Key: "time", Value: 1535530440},
-			},
-			bson.D{
-				{Key: "val", Value: 80},
-				{Key: "time", Value: 1535530480},
-			}},
-		},
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(locationAdd)*/
-	opts := options.Update().SetUpsert(true)
-	filter := bson.D{{Key: "sensorID", Value: sensorID}, {Key: "numSamples", Value: bson.D{{Key: "$lt", Value: 250}}}}
-	update := bson.D{
-		{Key: "$push", Value: bson.D{
-			{Key: "samples", Value: bson.D{
-				{Key: "val", Value: temp},
-				{Key: "time", Value: time.Now().UnixNano()},
-			}}},
-		},
-		{Key: "$inc", Value: bson.D{{Key: "numSamples", Value: 1}}},
-	}
-	locationUpdate, err := locations.UpdateOne(ctx, filter, update, opts)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(locationUpdate)
-}
-func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		port = "5000"
 	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello Sensor")
@@ -91,8 +59,57 @@ func main() {
 		temp, _ := strconv.ParseFloat(vars["temp"], 64)
 
 		fmt.Fprintf(w, "pushed sensor: %d with temp %f\n", sensorID, temp)
-		dbHandled(temp, sensorID)
-	})
+		opts := options.Update().SetUpsert(true)
+		filter := bson.D{{Key: "sensorID", Value: sensorID}, {Key: "numSamples", Value: bson.D{{Key: "$lt", Value: 250}}}}
+		update := bson.D{
+			{Key: "$push", Value: bson.D{
+				{Key: "samples", Value: bson.D{
+					{Key: "val", Value: temp},
+					{Key: "time", Value: time.Now().UnixNano()},
+				}}},
+			},
+			{Key: "$inc", Value: bson.D{{Key: "numSamples", Value: 1}}},
+		}
+		locationUpdate, err := locations.UpdateOne(ctx, filter, update, opts)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(locationUpdate)
+	}).Methods("GET")
+
+	r.HandleFunc("/sensor/{sensorID}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		sensorID, _ := strconv.Atoi(vars["sensorID"])
+		var values []bson.M
+		cursor, err := locations.Find(ctx, bson.M{"sensorID": bson.D{{"$eq", sensorID}}})
+		if err != nil {
+			panic(err)
+		}
+		if err = cursor.All(ctx, &values); err != nil {
+			panic(err)
+		}
+		for _, value := range values {
+			fmt.Println(value["numSamples"])
+		}
+
+		fmt.Fprintf(w, "Reading MongoDB\n")
+	}).Methods("GET")
+
+	r.HandleFunc("/new", func(w http.ResponseWriter, r *http.Request) {
+		sensor := sensor{
+			sensorID:   1,
+			numSamples: 250,
+			day:        time.Now().Format("01-02-2006"),
+			first:      0,
+			last:       0,
+			samples:    []sample{{0, 0}},
+		}
+		insertResult, err := locations.InsertOne(ctx, sensor)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(insertResult.InsertedID)
+	}).Methods("GET")
 
 	http.ListenAndServe(":"+port, r)
 }
